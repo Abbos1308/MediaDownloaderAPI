9bd17326c1bfe8e6f11cb047d87737ac9ac87aab
Abbos1308 <124450651+Abbos1308@users.noreply.github.com>
2024-03-04 22:22:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-const {searchManager,downloadManager} = require('ytmusic_api_unofficial')
-
 app.get("/", (req, res) => {
   res.send("This is API for download any media from social networks");
 });
@@ -14,16 +12,4 @@ app.get("/instagram/", (req, res) => {
     res.json(result);
   });
 });
-
-
-
-
-app.get("/yt_music",(req,res)=> {
-    q = req.query.q;
-    searchManager.search(q, 'VIDEO').then((result) => {
-        const id = result[0].id;
-        const song_id = result[0].id;
-        res.json({"url":"https://www.youtube.com/watch?v="+song_id});
-    });
-});
-
+app.listen(3000);
