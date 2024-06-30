03a34d550a10c01acdba4e36dc78d45db68c37c8
Abbos1308 <124450651+Abbos1308@users.noreply.github.com>
2024-06-30 13:55:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -25,16 +25,17 @@ app.get("/instagram/", (req, res) => {
 });
 
 // YouTube Music route
-app.get("/yt_music", (req, res) => {
-  const q = req.query.q;
-  searchManager.search(q, 'MUSIC')
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((error) => {
-      console.error("Error occurred while searching YouTube Music:", error);
-      res.status(500).json({ error: "An error occurred while searching YouTube Music" });
-    });
+app.get("/tiktok", (req, res) => {
+  const link = req.query.link;
+  const Tiktok = require("tiktokmediasaver")
+
+  const tiktok_url = "https://www.tiktok.com/@defnite_ilm_2812/video/7362467772422425861?is_from_webapp=1&sender_device=mobile&sender_web_id=7384300034148582945"
+
+  Tiktok.Downloader(tiktok_url, {
+    version: "v2" //  version: "v1" | "v2" | "v3"
+  }).then((result) => {
+    res.json(result);
+  })
 });
 
 const port = process.env.PORT || 3000;
